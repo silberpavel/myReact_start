57f90ab48fa81b962b49f940ed8553978faaf7d4
Silber <silber.ypm@gmail.com>
2018-01-21 06:30:39 +0500

    Here we define function called Hero() than returns JSX elements.

--- app.jsx
+++ app.jsx
@@ -1,8 +1,16 @@
-var jsx = 
-    <div className="container">
-        <img src="./img/react.png" />
-        <h1>React</h1>
-        <p>Cool library for user interfaces creation</p>
-    </div>;
+// JSX >> JS Transpilation
 
-ReactDOM.render(jsx, document.getElementById('root'));
+
+// Here we define function than returns JSX elements
+function Hero() {
+    return (
+        <div className="container">
+            <img src="./img/react.png" />
+            <h1>React</h1>
+            <p>Cool library for user interfaces creation</p>
+        </div>
+    );
+}
+
+// ReactDOM.render(React.createElement(Hero, null), document.getElementById('root'));
+ReactDOM.render(<Hero />, document.getElementById('root'));
