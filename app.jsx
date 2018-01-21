3c64988d041188f3e291af102bc510fb2fc14478
Silber <silber.ypm@gmail.com>
2018-01-21 06:41:15 +0500

    PROPS using (property of an element).

--- app.jsx
+++ app.jsx
@@ -2,15 +2,18 @@
 
 
 // Here we define function than returns JSX elements
-function Hero() {
+function Hero(props) {
     return (
         <div className="container">
-            <img src="./img/react.png" />
-            <h1>React</h1>
-            <p>Cool library for user interfaces creation</p>
+            <img src={props.imageURL} />
+            <h1>{props.title}</h1>
+            <p>{props.subtitle}</p>
         </div>
     );
 }
 
 // ReactDOM.render(React.createElement(Hero, null), document.getElementById('root'));
-ReactDOM.render(<Hero />, document.getElementById('root'));
+ReactDOM.render(<Hero title="React"
+                      subtitle="Cool library for user interfaces creation"
+                      imageURL="./img/react.png"/>,
+document.getElementById('root'));
